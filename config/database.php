8172fe71d1415c7180cc1e8eb47a9e89be06d093
kaledmolina <kaledmoly@gmail.com>
2026-01-16 16:21:46 -0500

    config: Update database host from localhost to 127.0.0.1.

--- config/database.php
+++ config/database.php
@@ -1,6 +1,6 @@
 <?php
 // Configuración de la base de datos
-define('DB_HOST', 'localhost');
+define('DB_HOST', '127.0.0.1');
 define('DB_USER', 'root');
 define('DB_PASS', '');
 define('DB_NAME', 'inventario_ti');
